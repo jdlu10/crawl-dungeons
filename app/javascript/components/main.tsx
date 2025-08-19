38af2b0cae5ffba8f8c1ed055907be457479948c
Jwun-Da Lu <jdlu10@gmail.com>
2025-08-19 00:49:20 -0400

    debugg

--- app/javascript/components/main.tsx
+++ app/javascript/components/main.tsx
@@ -50,6 +50,7 @@ export default function Main() {
     <>
       {renderGame()}
       <pre className="debugging absolute w-150 h-150 right-0 bottom-0 overflow-scroll">
+        try
         {JSON.stringify(party, null, 2)}
         {JSON.stringify(battle, null, 2)}
       </pre>
